e598d1393350f2228d240217a1fe4ddbee897709
Yann Caron <cyann74@gmail.com>
2015-11-10 16:52:09 +0100

    Iterator tests

--- src/test/java/fr/cyann/jinyparser/tree/DepthFirstIteratorTest.java
+++ src/test/java/fr/cyann/jinyparser/tree/DepthFirstIteratorTest.java
@@ -10,31 +10,33 @@ import java.util.List;
 import static fr.cyann.jinyparser.grammartree.GrammarFactory.*;
 
 /**
- * Copyright (C) 09/11/15 Yann Caron aka cyann
+ * Copyright (C) 10/11/15 Yann Caron aka cyann
  * <p/>
  * Cette œuvre est mise à disposition sous licence Attribution -
  * Pas d’Utilisation Commerciale - Partage dans les Mêmes Conditions 3.0 France.
  * Pour voir une copie de cette licence, visitez http://creativecommons.org/licenses/by-nc-sa/3.0/fr/
  * ou écrivez à Creative Commons, 444 Castro Street, Suite 900, Mountain View, California, 94041, USA.
  **/
-public class AscendingIteratorTest extends TestCase {
+public class DepthFirstIteratorTest extends TestCase {
 
 	public void testIterator() throws Exception {
 
-		GrammarElement g3 = word("a");
-		GrammarElement g2 = oneOrMore(g3);
-		GrammarElement g1 = sequence(g2, word("b"));
+		GrammarElement g33 = word("c");
+		GrammarElement g32 = word("b");
+		GrammarElement g31 = word("a");
+		GrammarElement g22 = sequence(g32, g33);
+		GrammarElement g21 = oneOrMore(g31);
+		GrammarElement g1 = sequence(g21, g22);
 		GrammarElement g0 = recursive("a").setGrammar(g1);
 
 		List<GrammarElement> result = new ArrayList<GrammarElement>();
 
-		for (GrammarElement g : g3.ascendingTraversal()) {
+		for (GrammarElement g : g0.depthFirstTraversal()) {
 			result.add(g);
 		}
 
 		System.out.println(result);
-		assertEquals(Arrays.asList(g3, g2, g1, g0), result);
+		assertEquals(Arrays.asList(g0, g1, g21, g31, g22, g32, g33), result);
 
 	}
-
 }
